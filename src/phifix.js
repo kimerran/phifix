7f18868c3e98becc45fbdac788601af9ae4ddb73
kimerran <mh.neri@gmail.com>
2014-05-15 12:49:17 +0800

    fixes #1

--- src/phifix.js
+++ src/phifix.js
@@ -8,9 +8,9 @@ function Phifix(){
 		network_name_globe: "Globe",
 	};
 	this.find = function(prefix){
-		if (this.config.prefixes_sun.contains(prefix)) return this.config.network_name_sun;
-		if (this.config.prefixes_smart.contains(prefix)) return this.config.network_name_smart;
-		if (this.config.prefixes_globe.contains(prefix)) return this.config.network_name_globe;			
+		if (this.config.prefixes_sun.indexOf(prefix) > -1) return this.config.network_name_sun;
+		if (this.config.prefixes_smart.indexOf(prefix) > -1) return this.config.network_name_smart;
+		if (this.config.prefixes_globe.indexOf(prefix) > -1) return this.config.network_name_globe;			
 	}
 	this.setNetworkName = function(netid,name){
 		switch(netid) {
@@ -35,13 +35,6 @@ function Phifix(){
 	this.config.prefixes_globe =  ['817','905','906','915','916','917','926','927','935','936','937','994','996','997'];
 	this.config.prefixes_sun =  ['922','923','925','932','933','934','942','943'];
 };
-//some helper methods
-Array.prototype.contains = function(k) {
-    for(var p in this)
-    	if(this[p] === k)
-            return true;
-    return false;
-}
 
 window.phifix = new Phifix();
 
